81262505c66ad176156fb6e3000376ee8e7ca842
DydikN <PingvinPain@gmail.com>
2023-04-10 18:37:26 +0300

    first try

--- app.js
+++ app.js
@@ -1,9 +1,69 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
+var age;
+var namep;
+var toggle;
+var empty;
+var notInitialize;
+var callback;
+age = 50;
+namep = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
+callback = function (a) {
+    return 100 + a;
+};
+var anything;
+anything = -20;
+anything = "Text";
+anything = {};
+var some;
+some = "Text";
+var str;
+if (typeof some === "string") {
+    str = some;
+}
+var person;
+person = ["Max", 21];
+// func
+var Load;
+(function (Load) {
+    Load[Load["LOADING"] = 0] = "LOADING";
+    Load[Load["READY"] = 1] = "READY";
+})(Load || (Load = {}));
+var page = {
+    state: Load.LOADING,
+};
+if (page.state === Load.LOADING) {
+    console.log("Сторінка завантажується");
+}
+if (page.state === Load.READY) {
+    console.log("Сторінка завантажена");
+}
+//
+var state;
+var literal;
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
     return num1 + num2;
 }
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+function customError() {
+    throw new Error("Error");
+}
+var page1 = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: "2021-01-01",
+        updateAt: "2021-05-01",
+    },
+};
+var page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close",
+};
--- app.ts
+++ app.ts
@@ -1,11 +1,96 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+let age: number;
+let namep: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+let callback: (a: number) => number;
 
-function add(num1: number, num2: number) {
+age = 50;
+namep = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
+callback = (a) => {
+  return 100 + a;
+};
+
+let anything: any;
+anything = -20;
+anything = "Text";
+anything = {};
+
+let some: unknown;
+some = "Text";
+
+let str: string;
+
+if (typeof some === "string") {
+  str = some;
+}
+
+let person: [string, number];
+person = ["Max", 21];
+
+// func
+
+enum Load {
+  LOADING,
+  READY,
+}
+const page = {
+  state: Load.LOADING,
+};
+
+if (page.state === Load.LOADING) {
+  console.log("Сторінка завантажується");
+}
+
+if (page.state === Load.READY) {
+  console.log("Сторінка завантажена");
+}
+//
+
+let state: string | number;
+
+let literal: "enable" | "disable";
+
+function showMessage(message: string) {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number) {
   return num1 + num2;
 }
 
-button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
-});
+function customError(): never {
+  throw new Error("Error");
+}
+
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
+const page1 = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: "2021-01-01",
+    updateAt: "2021-05-01",
+  },
+};
+
+const page2 = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
